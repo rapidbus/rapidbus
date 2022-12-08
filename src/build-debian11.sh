79c693a54bd4d4e07afb536058fd8010c52b5c5e
Marcel Hecko <maco@blava.net>
2022-12-08 12:27:03 +0100

    add docker building scripts

---
+++ src/build-debian11.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -f Dockerfile-Debian11 -t rapidbus/build-debian11 .
+docker run -it -v `pwd`:/opt/rapidbus --entrypoint /bin/bash rapidbus/build-debian11 -c "cd /opt/rapidbus/; make all"
---
+++ src/build-rhel8.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -f Dockerfile-RHEL8 -t rapidbus/build-rhel8 .
+docker run -it -v `pwd`:/opt/rapidbus --entrypoint /bin/bash rapidbus/build-rhel8 -c "cd /opt/rapidbus/; make all"

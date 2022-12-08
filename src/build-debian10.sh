a32142a8f06d4959ea81a690c4c72fe2cff006a5
marcel <marcel@hector.blava.net>
2022-12-08 16:08:12 -0500

    fix codacy issues

--- src/build-debian10.sh
+++ src/build-debian10.sh
@@ -3,4 +3,4 @@
 set -e
 
 docker build -f Dockerfile-Debian10 -t rapidbus/build-debian10 .
-docker run -it -v `pwd`:/opt/rapidbus --entrypoint /bin/bash rapidbus/build-debian10 -c "cd /opt/rapidbus/; make all"
+docker run -it -v $(pwd):/opt/rapidbus --entrypoint /bin/bash rapidbus/build-debian10 -c "cd /opt/rapidbus/; make all"

96b304fc14b6b3418f8cc35042524a79320109e8
Marcel Hecko <maco@blava.net>
2022-12-08 12:37:26 +0100

    add rhel9 build env

--- src/beautify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-#python -m black -l 120 *.py
-#python -m pylint *.py
-
-scan-build-13 --status-bugs make
---
+++ src/build-rhel9.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -f Dockerfile-RHEL9 -t rapidbus/build-rhel9 .
+docker run -it -v `pwd`:/opt/rapidbus --entrypoint /bin/bash rapidbus/build-rhel9 -c "cd /opt/rapidbus/; make all"

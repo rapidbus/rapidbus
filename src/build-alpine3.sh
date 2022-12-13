511462e75022f0dab977a5e49fbb24afb2378789
Marcel Hecko <marcel.hecko@plasticomnium.com>
2022-12-13 16:16:58 +0100

    extract version to file

--- src/build-alpine3.sh
+++ src/build-alpine3.sh
@@ -7,3 +7,4 @@ set -e
 
 $DENGINE build -f Dockerfile-Alpine3 -t rapidbus/build-alpine3 .
 $DENGINE run -it -v $(pwd):/opt/rapidbus --entrypoint /bin/sh rapidbus/build-alpine3 -c "cd /opt/rapidbus/; make all; ./rapidbusd -h"
+tar -czfv rapidbusd.tar.gz rapidbusd rapidbusd.conf.example
